d99467e0640753a0e7977f05a3c1597c1ee2d169
atctsim <antonytsim@gmail.com>
2019-01-20 01:24:10 -0500

    Clean file up

--- database.js
+++ database.js
@@ -9,35 +9,6 @@ let config = {
   password: process.env.SQL_PASSWORD,
 }
 
-/*
-if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
- config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
-}
-*/
-
 let connection = mysql.createConnection(config);
 
-//Database Connection for Development
-/*
-let connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASS,
-  //socketPath: `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`
-});
-
-//if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
-// config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
-//}
-
-
-  connection.connect(function(err) {
-    if (err) {
-      console.error('Error connecting: ' + err.stack);
-      return;
-    }
-    console.log('Connected as thread id: ' + connection.threadId);
-  });
-*/
   module.exports = connection;
